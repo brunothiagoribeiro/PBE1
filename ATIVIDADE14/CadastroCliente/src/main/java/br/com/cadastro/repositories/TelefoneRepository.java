4eaa11cf8000b350a56dc23d2fc3953fe3efa3ea
brunothiagoribeiro <159180405+brunothiagoribeiro@users.noreply.github.com>
2024-11-19 12:24:56 -0300

    Delete ATIVIDADE14 directory

--- ATIVIDADE14/CadastroCliente/src/main/java/br/com/cadastro/repositories/TelefoneRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.cadastro.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.cadastro.entities.Telefone;
-
-public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
-
-}
