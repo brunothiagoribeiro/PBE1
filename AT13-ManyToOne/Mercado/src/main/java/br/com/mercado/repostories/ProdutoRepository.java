afefb5edbcece2719c03fed00bb430ee34a7d862
brunothiagoribeiro <159180405+brunothiagoribeiro@users.noreply.github.com>
2024-11-19 08:45:16 -0300

    Delete AT13-ManyToOne/Mercado directory

--- AT13-ManyToOne/Mercado/src/main/java/br/com/mercado/repostories/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.mercado.repostories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.mercado.entities.Produto;
-
-public interface ProdutoRepository extends  JpaRepository<Produto, Long> {
-
-}
